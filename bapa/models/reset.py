a84b7b9ac0984695f6edf0f93b8a508248b88951
kelleydv <kelleydv@gmail.com>
2015-03-03 11:39:57 -0800

    delete superfluous salt

--- bapa/models/reset.py
+++ bapa/models/reset.py
@@ -9,7 +9,6 @@ class Reset(Base):
         self.collection = self.db.resets
 
     def create(self, user_id, timestamp, secret):
-        salt = get_salt()
         return self.collection.insert({
             'user_id': user_id,
             'timestamp': timestamp,
