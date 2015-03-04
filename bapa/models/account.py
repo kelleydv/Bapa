e5383ae9ede5440c59e0b5bb024b9ac1d4f6b604
kelleydv <kelleydv@gmail.com>
2015-03-03 18:25:13 -0800

    static variables and class methods

--- bapa/models/account.py
+++ bapa/models/account.py
@@ -2,22 +2,21 @@ from bapa.utils import timestamp
 from bapa.models import Base
 
 class Account(Base):
-    """
-    For modeling user financial contributions
-    """
-    def __init__(self):
-        self.init_db()
-        self.collection = self.db.accounts
+    """For modeling user financial contributions"""
+    
+    collection = Base.db.accounts
 
-    def create(self, user_id, amount):
-        return self.collection.insert({
+    @classmethod
+    def create(cls, user_id, amount):
+        return cls.collection.insert({
             'user_id': user_id,
             'amount': amount,
             'date': timestamp(),
         })
 
-    def from_user(self, user_id):
-        return self.collection.find_one(
+    @classmethod
+    def from_user(cls, user_id):
+        return cls.collection.find_one(
             {'user_id': user_id}
         )
 
