02e7a041da776fed69bd79480e8ac478f7e78525
kelleydv <kelleydv@gmail.com>
2015-03-07 21:42:45 -0800

    move slash

--- bapa/apis/ushpa.py
+++ bapa/apis/ushpa.py
@@ -4,7 +4,7 @@ import requests
 def get_pilot_data(ushpa):
     """Access the USHPA API and return a dictionary of data"""
     req = app.config['USHPA_HOST']
-    req += 'ushpa_validation_ratings.asp?chapter=%s&pin=%s&ushpa=%s' 
+    req += '/ushpa_validation_ratings.asp?chapter=%s&pin=%s&ushpa=%s' 
     data = requests.get( req % (app.config['USHPA_CHAPTER'], app.config['USHPA_PIN'], ushpa) )
     #Parse plain text
     data = ( x for x in data.text.strip().split('\n') )
