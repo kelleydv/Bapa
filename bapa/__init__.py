eed53923b557dc766c2e35d6434dad50d3f61c79
kelleydv <kelleydv@gmail.com>
2015-03-07 17:46:36 -0800

    implement config file

--- bapa/__init__.py
+++ bapa/__init__.py
@@ -1,22 +1,10 @@
-import os
 from flask import Flask, url_for
 from flask_mail import Mail
+from flask import json
 
 app = Flask(__name__)
 
-app.config.update(
-    DEBUG = True,
-    SECRET_KEY = os.environ['bapa_session_secret'],
-
-    MAIL_SERVER = os.environ['bapa_mail_server'],
-    MAIL_PORT = os.environ['bapa_mail_port'],
-    MAIL_USERNAME = os.environ['bapa_mail_username'],
-    MAIL_PASSWORD = os.environ['bapa_mail_password'],
-    MAIL_DEFAULT_SENDER = os.environ['bapa_mail_sender'],
-
-    USHPA_CHAPTER = os.environ['bapa_ushpa_chapter'],
-    USHPA_PIN = os.environ['bapa_ushpa_pin']
-)
+app.config.from_envvar('BAPA_CONF')
 
 mail = Mail(app)
 
