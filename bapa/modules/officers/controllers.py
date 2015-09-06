637e4476659c1d1d5488185e20707a7a4577285c
kelleydv <kelleydv@gmail.com>
2015-09-06 10:34:20 -0700

    better view

--- bapa/modules/officers/controllers.py
+++ bapa/modules/officers/controllers.py
@@ -1,4 +1,14 @@
 from bapa import models
+from bapa.utils import object_from_timestamp, is_too_old
+
+def _is_member(id):
+    dues = models.Payment.latest(user_id=id, item='Membership Dues')
+    if dues:
+        t = object_from_timestamp(dues[0]['timestamp'])
+        if not is_too_old(t, years=1):
+            return True
+        return False
+
 
 def get_members():
     """
@@ -6,5 +16,6 @@ def get_members():
     for each registered user of the site.
     """
     users =  models.User.match_all()
-    members = [(x['firstname'], x['lastname'], x['ushpa_data'].get('pg_pilot_rating')) for x in users]
+    f = lambda a: 'Active' if _is_member(a) else 'Not Active'
+    members = [(x['firstname'], x['lastname'], x['ushpa_data'].get('pg_pilot_rating'), f(x['_id'])) for x in users]
     return members
