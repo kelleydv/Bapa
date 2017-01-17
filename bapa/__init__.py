a1e6c3a843906574fdb304d2e2d76a9c2740c46b
kelleydv <kelleydv@gmail.com>
2017-01-17 10:05:44 -0800

    no git repo on heroku

--- bapa/__init__.py
+++ bapa/__init__.py
@@ -27,6 +27,4 @@ for prefix, bp in blueprints.items():
 
 # Template Globals
 app.jinja_env.globals['year'] = date.today().year
-head = str(subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip(), "utf-8")
-app.jinja_env.globals['commit_hash'] = head[:4]
 app.jinja_env.globals['env'] = env.lower()
